548d561e3ed49eed3b15d0ecbb14f8d72540d2c9
newusdharamshala <newusdshala@gmail.com>
2025-06-08 22:22:59 +0530

    portt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
