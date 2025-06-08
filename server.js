52bb3b2069a7004da3f2f404a0fc7f82d1579ec1
newusdharamshala <newusdshala@gmail.com>
2025-06-08 14:26:16 +0530

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +15,6 @@ mongoose
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/donations", require("./routes/donations"));
 
-app.listen(post => {
-  console.log("Server running on port 5500");
+app.listen((port) => {
+  console.log(`Server running on port ${port}`);
 });
